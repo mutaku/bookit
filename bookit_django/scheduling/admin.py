d266aea671f412293a499e69fa1da8cd8500fbbd
Matthew Martz <xiao_haozi@mutaku.com>
2016-03-04 15:48:46 -0500

    few pep things

--- bookit_django/scheduling/admin.py
+++ bookit_django/scheduling/admin.py
@@ -13,20 +13,20 @@ from .utils import changed_event_mail, deleted_event_mail,\
 
 
 def toggle_boolean(modeladmin, request, queryset, field):
-    '''Toggle the boolean value of a model field'''
+    """Toggle the boolean value of a model field"""
     for obj in queryset:
         setattr(obj, field, not getattr(obj, field))
         obj.save()
 
 
 # class EventForm(forms.ModelForm):
-#     '''Tweak event form for validation'''
+#     """Tweak event form for validation"""
 #     def __init__(self, *args, **kwargs):
 #         self.user = kwargs.pop('user', None)
 #         super(EventForm, self).__init__(*args, **kwargs)
 #
 #     def clean(self):
-#         '''Tweak cleaning validation'''
+#         """Tweak cleaning validation"""
 #         if self.user not in self.equipment.users.all():
 #             raise ValidationError(
 #               'You do not have access to this instrument.')
@@ -35,7 +35,7 @@ def toggle_boolean(modeladmin, request, queryset, field):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    '''Tweak the Event admin form'''
+    """Tweak the Event admin form"""
 
     list_display = ('start_time',
                     'end_time',
@@ -49,14 +49,14 @@ class EventAdmin(admin.ModelAdmin):
     actions = ['cancel_event']
 
     def get_list_filter(self, request):
-        '''Tweak list filtering based on user'''
+        """Tweak list filtering based on user"""
         if request.user.is_superuser:
             return ['start_time', 'user',
                     'equipment', 'maintenance']
         return ['equipment']
 
     def get_list_display(self, request):
-        '''Tweak list display based on user'''
+        """Tweak list display based on user"""
         list_display = ('start_time',
                         'end_time',
                         'elapsed_hours',
@@ -69,17 +69,17 @@ class EventAdmin(admin.ModelAdmin):
         return list_display
 
     def cancel_event(self, request, queryset):
-        '''Cancel an upcoming event'''
+        """Cancel an upcoming event"""
         queryset.filter(status='A', expired=False).update(status='C')
 
     def has_delete_permission(self, request, obj=None):
-        '''Adjust deletion permissions to use cancel'''
+        """Adjust deletion permissions to use cancel"""
         if request.user.is_superuser:
             return True
         return False
 
     # def formfield_for_choice_field(self, db_field, request, **kwargs):
-    #     '''Overide choice fields based on conditions'''
+    #     """Override choice fields based on conditions"""
     #     if db_field.name == 'status' and not request.user.is_superuser:
     #         kwargs['choices'] = (
     #             ("C", "Canceled"),
@@ -88,14 +88,13 @@ class EventAdmin(admin.ModelAdmin):
     #         db_field, request, **kwargs)
 
     def get_fields(self, request, obj=None, **kwargs):
-        '''Overide field getting'''
+        """Override field getting"""
         if not request.user.is_superuser:
-            # self.exclude = ('status', 'maintenance',)
             self.exclude = ('maintenance', 'expired', 'status',)
         return super(EventAdmin, self).get_fields(request, obj, **kwargs)
 
     def get_form(self, request, obj=None, **kwargs):
-        '''Overide form getting'''
+        """Override form getting"""
         form = super(EventAdmin, self).get_form(request, obj, **kwargs)
         form.base_fields['equipment'].queryset = \
             form.base_fields['equipment'].\
@@ -103,7 +102,12 @@ class EventAdmin(admin.ModelAdmin):
         return form
 
     def save_model(self, request, obj, form, change):
-        '''Adjust some values on save'''
+        """Adjust some values on save"""
+        event_functions = {
+            'new_event': new_event_mail,
+            'maintenance': maintenance_announcement,
+            'changed_event': changed_event_mail
+        }
         if getattr(obj, 'user', None) is None:
             obj.user = request.user
         if obj.user not in obj.equipment.users.all():
@@ -113,38 +117,50 @@ class EventAdmin(admin.ModelAdmin):
         elapsed_td = (obj.end_time - obj.start_time).seconds
         obj.elapsed_hours = round(float(elapsed_td / 3600.0), 2)
         if obj.pk is None and not obj.maintenance:
-            self.message_user(request,
-                              "Created {}".format(obj.start_timestring),
-                              messages.SUCCESS)
-            new_event_mail(obj)
-        if obj.pk is None and obj.maintenance and request.user.is_superuser:
-            self.message_user(request,
-                              "Maintence scheduled {}".format(
-                                obj.start_timestring),
-                              messages.SUCCESS)
-            maintenance_announcement(obj)
+            # self.message_user(request,
+            #                   "Created {}".format(obj.start_timestring),
+            #                   messages.SUCCESS)
+            # new_event_mail(obj)
+            save_method = 'new_event'
+            save_method_string = "Created {}".format(obj.start_timestring)
+        elif obj.maintenance and request.user.is_superuser:
+            # self.message_user(request,
+            #                   "Maintence scheduled {}".format(
+            #                     obj.start_timestring),
+            #                   messages.SUCCESS)
+            # maintenance_announcement(obj)
+            save_method = 'maintenance'
+            save_method_string = "Maintence scheduled {}".format(
+                obj.start_timestring)
         elif ((any(f in ['start_time', 'end_time'] for
                    f in form.changed_data) and
                all(x is not None for x in
                    [obj.orig_start, obj.orig_end])) or
               (('status' in form.changed_data) and
                 obj.status == 'C')):
-            self.message_user(request,
-                              "changed from {} - {}".format(obj.orig_start,
-                                                            obj.orig_end),
-                              messages.SUCCESS)
-            changed_event_mail(obj)
+            # self.message_user(request,
+            #                   "changed from {} - {}".format(obj.orig_start,
+            #                                                 obj.orig_end),
+            #                   messages.SUCCESS)
+            # changed_event_mail(obj)
+            save_method = 'changed_event'
+            save_method_string = "changed from {} - {}".format(
+                obj.orig_start, obj.orig_end)
         super(EventAdmin, self).save_model(request, obj, form, change)
+        event_functions[save_method](obj)
+        self.message_user(request,
+                          save_method_string,
+                          messages.SUCCESS)
 
     def get_queryset(self, request):
-        '''Override the queryset to enforce permissions'''
+        """Override the queryset to enforce permissions"""
         qstring = super(EventAdmin, self).get_queryset(request)
         if request.user.is_superuser:
             return qstring
         return qstring.filter(user=request.user)
 
     def delete_model(self, request, obj):
-        '''Prevent deleting events that have already occurred'''
+        """Prevent deleting events that have already occurred"""
         if getattr(obj, 'end_time', None) <= timezone.now():
             raise PermissionDenied
         self.message_user(request, "Deleted {}".format(obj.id),
@@ -153,7 +169,7 @@ class EventAdmin(admin.ModelAdmin):
         super(EventAdmin, self).delete_model(request, obj)
 
     def response_add(self, request, obj, post_url_continue=None):
-        '''Redirect back to main view after successful add'''
+        """Redirect back to main view after successful add"""
         self.message_user(request, 'Added {}'.format(obj.start_timestring),
                           messages.SUCCESS)
         return HttpResponseRedirect(
@@ -164,7 +180,7 @@ class EventAdmin(admin.ModelAdmin):
                     obj.start_time.month)))
 
     def response_change(self, request, obj):
-        '''Redirect back to main view after successful edit'''
+        """Redirect back to main view after successful edit"""
         self.message_user(request, 'Edited {}'.format(obj.start_timestring),
                           messages.SUCCESS)
         return HttpResponseRedirect(
@@ -175,7 +191,7 @@ class EventAdmin(admin.ModelAdmin):
                     obj.start_time.month)))
 
     def response_delete(self, request, obj_name, obj_id):
-        '''Redirect back to main view after successful delete'''
+        """Redirect back to main view after successful delete"""
         self.message_user(request, 'Deleted {} - {}'.format(obj_name,
                                                             obj_id),
                           messages.SUCCESS)
@@ -209,14 +225,14 @@ class EventAdmin(admin.ModelAdmin):
 
 
 class ComponentInline(admin.TabularInline):
-    '''Components to attach to equipment'''
+    """Components to attach to equipment"""
     model = Equipment.component.through
     extra = 0
     can_delete = True
 
 
 # class ServiceInline(admin.TabularInline):
-#     '''Service record for equipment'''
+#     """Service record for equipment"""
 #     model = Equipment.service_record.through
 #     extra = 0
 #     can_delete = False
@@ -224,7 +240,7 @@ class ComponentInline(admin.TabularInline):
 
 @admin.register(Component)
 class ComponentAdmin(admin.ModelAdmin):
-    '''Component management'''
+    """Component management"""
     #exclude = ('brand', 'model',)
     #inlines = [BrandInline, ModelInline]
     pass
@@ -232,7 +248,7 @@ class ComponentAdmin(admin.ModelAdmin):
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    '''Service record management'''
+    """Service record management"""
     exclude = ('user',)
     list_display = ('date', 'user', 'equipment',
                     'component', 'short_job_title',
@@ -241,15 +257,15 @@ class ServiceAdmin(admin.ModelAdmin):
     actions = ['toggle_completed', 'toggle_success']
 
     def toggle_success(self, request, queryset):
-        '''Toggle ticket priority'''
+        """Toggle ticket priority"""
         toggle_boolean(self, request, queryset, 'success')
 
     def toggle_completed(self, request, queryset):
-        '''Toggle ticket priority'''
+        """Toggle ticket priority"""
         toggle_boolean(self, request, queryset, 'completed')
 
     def save_model(self, request, obj, form, change):
-        '''Adjust some values on save'''
+        """Adjust some values on save"""
         if getattr(obj, 'user', None) is None:
             obj.user = request.user
         super(ServiceAdmin, self).save_model(request, obj, form, change)
@@ -257,21 +273,22 @@ class ServiceAdmin(admin.ModelAdmin):
 
 @admin.register(Equipment)
 class EquipmentAdmin(admin.ModelAdmin):
-    '''Equipment management'''
+    """Equipment management"""
 
-    list_display = ('name', 'brand', 'model', 'description')
+    list_display = ('name', 'brand', 'model',
+                    'description', 'last_service', 'status')
     inlines = [ComponentInline]
     exclude = ('component',)
 
     def get_fields(self, request, obj=None, **kwargs):
-        '''Overide field getting'''
+        """Override field getting"""
         if not request.user.is_superuser:
             self.readonly_fields = ('admin',)
         return super(EquipmentAdmin, self).get_fields(request, obj, **kwargs)
 
 
 class CommentInline(admin.TabularInline):
-    '''Comments to attach to a ticket'''
+    """Comments to attach to a ticket"""
     model = Ticket.comment.through
     extra = 0
     can_delete = False
@@ -286,19 +303,19 @@ class CommentInline(admin.TabularInline):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    '''Comment admin'''
+    """Comment admin"""
     # inlines = [CommentInline,]
     exclude = ('comment',)
 
     def get_queryset(self, request):
-        '''Override the queryset to enforce permissions'''
+        """Override the queryset to enforce permissions"""
         qstring = super(CommentAdmin, self).get_queryset(request)
         if request.user.is_superuser:
             return qstring
         return qstring.filter(user=request.user)
 
     def save_model(self, request, obj, form, change):
-        '''Adjust some values on save'''
+        """Adjust some values on save"""
         if getattr(obj, 'user', None) is None and obj.pk is None:
             obj.user = request.user
         elif (obj.pk and obj.user != request.user and
@@ -307,7 +324,7 @@ class CommentAdmin(admin.ModelAdmin):
         super(CommentAdmin, self).save_model(request, obj, form, change)
 
     def delete_model(self, request, obj):
-        '''Prevent deleting events that have already occurred'''
+        """Prevent deleting events that have already occurred"""
         if (obj.pk and obj.user != request.user and
                 not request.user.is_superuser):
             raise PermissionDenied
@@ -352,7 +369,7 @@ class CommentAdmin(admin.ModelAdmin):
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
-    '''Maintainence Request Ticket management'''
+    """Maintenance Request Ticket management"""
 
     list_display = ('created', 'user', 'equipment', 'priority',
                     'comment_count', 'msg', 'status')
@@ -362,24 +379,24 @@ class TicketAdmin(admin.ModelAdmin):
     actions = ['toggle_ticket', 'toggle_priority']
 
     def get_queryset(self, request):
-        '''Override the queryset to enforce permissions'''
+        """Override the queryset to enforce permissions"""
         qstring = super(TicketAdmin, self).get_queryset(request)
         if request.user.is_superuser:
             return qstring
         return qstring.filter(user=request.user)
 
     def toggle_ticket(self, request, queryset):
-        '''Toggle ticket closed status'''
+        """Toggle ticket closed status"""
         toggle_boolean(self, request, queryset, 'status')
         for obj in queryset:
             ticket_status_toggle_email(obj)
 
     def toggle_priority(self, request, queryset):
-        '''Toggle ticket priority'''
+        """Toggle ticket priority"""
         toggle_boolean(self, request, queryset, 'priority')
 
     def save_model(self, request, obj, form, change):
-        '''Adjust some values on save'''
+        """Adjust some values on save"""
         if getattr(obj, 'user', None) is None and obj.pk is None:
             obj.user = request.user
         elif (obj.pk and obj.user != request.user and
@@ -395,12 +412,12 @@ class TicketAdmin(admin.ModelAdmin):
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    '''Message management'''
+    """Message management"""
 
     list_display = ('created', 'user', 'msg')
 
     def save_model(self, request, obj, form, change):
-        '''Adjust some values on save'''
+        """Adjust some values on save"""
         if getattr(obj, 'user', None) is None and obj.pk is None:
             obj.user = request.user
         elif (obj.pk and obj.user != request.user and
@@ -412,7 +429,7 @@ class MessageAdmin(admin.ModelAdmin):
         super(MessageAdmin, self).save_model(request, obj, form, change)
 
     def delete_model(self, request, obj):
-        '''Prevent deleting events that have already occurred'''
+        """Prevent deleting events that have already occurred"""
         if (obj.pk and obj.user != request.user and
                 not request.user.is_superuser):
             raise PermissionDenied
