7b5a1b693b2ba1e0a8b6fed59e9e1607e0a09957
Matthew Martz <xiao_haozi@mutaku.com>
2016-06-02 09:05:46 -0400

    morning reminders now go out day before so as to allow time for day-before preparations

--- bookit_django/scheduling/management/commands/morning_reminders.py
+++ bookit_django/scheduling/management/commands/morning_reminders.py
@@ -4,25 +4,26 @@ from scheduling.utils import event_reminder_mail
 from datetime import datetime
 
 
-
 class Command(BaseCommand):
-    """Email reminder to users about upcoming events for today"""
+	"""Email reminder to users about upcoming events for next day.
+	Allows time for 'day before' preparations.
+	"""
 
-    help = "Sends reminder emails to users for events starting today"
-    requires_system_checks = False
+	help = "Sends reminder emails to users for events starting next day"
+	requires_system_checks = False
 
-    def handle(self, *args, **options):
-        events = Event.objects.filter(expired=False,
-                                      status__in=['A', 'H'],
-                                      equipment__status=True,
-                                      start_time__day=datetime.now().day)
-        self.stdout.write(self.style.SUCCESS(
-            "{} Reminders: Found [{}] events.".format(
-                datetime.now().strftime('%a %d-%b-%y %H-%M-%S'),
-                str(len(events)))))
-        for event in events:
-            event_reminder_mail(event)
-        self.stdout.write(self.style.SUCCESS(
-            '{} Reminders: Emails [{}] sent.'.format(
-                datetime.now().strftime('%a %d-%b-%y %H-%M-%S'),
-                len(events))))
+	def handle(self, *args, **options):
+		events = Event.objects.filter(expired=False,
+									  status__in=['A', 'H'],
+									  equipment__status=True,
+									  start_time__day=datetime.now().day + 1)
+		self.stdout.write(self.style.SUCCESS(
+			"{} Reminders: Found [{}] events.".format(
+				datetime.now().strftime('%a %d-%b-%y %H-%M-%S'),
+				str(len(events)))))
+		for event in events:
+			event_reminder_mail(event)
+		self.stdout.write(self.style.SUCCESS(
+			'{} Reminders: Emails [{}] sent.'.format(
+				datetime.now().strftime('%a %d-%b-%y %H-%M-%S'),
+				len(events))))
